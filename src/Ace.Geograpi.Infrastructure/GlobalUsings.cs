f1ade112e6f56962646f9c8270ca47aaee814643
Dimitrie Tataru <dimitriettr@gmail.com>
2024-02-20 16:01:12 +0200

    Add overrides for 'ContinentRepository', to explicitly show the implemented methods

--- src/Ace.Geograpi.Infrastructure/GlobalUsings.cs
+++ src/Ace.Geograpi.Infrastructure/GlobalUsings.cs
@@ -8,3 +8,5 @@ global using System;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
