56fde549db34905b722e442668b04fd5ffd18e3e
Dimitrie Tataru <dimitriettr@gmail.com>
2024-02-20 13:31:19 +0200

    Add overrides for 'ContinentController', to explicitly show the implemented methods

--- src/Ace.Geograpi.Web/Controllers/ContinentController.cs
+++ src/Ace.Geograpi.Web/Controllers/ContinentController.cs
@@ -1,7 +1,10 @@
 using Ace.Geograpi.Domain.Models;
 using Ace.Geograpi.Domain.QueryFilters;
 using Ace.Geograpi.Domain.Services;
+using CatNip.Domain.Query;
+using CatNip.Domain.Query.Sorting.Symbols;
 using CatNip.Presentation.Controllers;
+using CatNip.Presentation.Symbols;
 
 namespace Ace.Geograpi.Web.Controllers;
 
@@ -19,5 +22,74 @@ public sealed class ContinentController : AceController<IContinentService, Conti
     public sealed override async Task<IActionResult> GetAll(CancellationToken cancellation)
     {
         return await base.GetAll(cancellation);
+    }
+
+    [HttpGet]
+    [Route(DefaultRoutes.Count)]
+    [ProducesResponseType<int>((int)HttpStatusCode.OK)]
+    public sealed override async Task<IActionResult> Count(CancellationToken cancellation)
+    {
+        return await base.Count(cancellation);
+    }
+
+    [HttpGet]
+    [Route(DefaultRoutes.Filter)]
+    [ProducesResponseType<QueryResponse<ContinentModel>>((int)HttpStatusCode.OK)]
+    public sealed override async Task<IActionResult> Filter(
+        [FromQuery] int? page,
+        [FromQuery] int? size,
+        [FromQuery] string? sortBy,
+        [FromQuery] SortDirection? sortDirection,
+        [FromQuery] ContinentQueryFilter filter,
+        CancellationToken cancellation)
+    {
+        return await base.Filter(page, size, sortBy, sortDirection, filter, cancellation);
+    }
+
+    [HttpGet]
+    [Route(DefaultRoutes.FilterCount)]
+    [ProducesResponseType<int>((int)HttpStatusCode.OK)]
+    public sealed override async Task<IActionResult> FilterCount(
+        [FromQuery] ContinentQueryFilter filter, CancellationToken cancellation)
+    {
+        return await base.FilterCount(filter, cancellation);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType<ContinentModel>((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public sealed override async Task<IActionResult> GetById(
+        [FromRoute] int id, CancellationToken cancellation)
+    {
+        return await base.GetById(id, cancellation);
+    }
+
+    [HttpPost]
+    [ProducesResponseType<ContinentModel>((int)HttpStatusCode.Created)]
+    public sealed override async Task<IActionResult> Create(
+        [FromBody] ContinentModel model, CancellationToken cancellation)
+    {
+        return await base.Create(model, cancellation);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public sealed override async Task<IActionResult> Update(
+        [FromRoute] int id, [FromBody] ContinentModel model, CancellationToken cancellation)
+    {
+        return await base.Update(id, model, cancellation);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public sealed override async Task<IActionResult> Delete(
+        [FromRoute] int id, CancellationToken cancellation)
+    {
+        return await base.Delete(id, cancellation);
     }
 }
