31746db3dc3acb5a4df9fb146e144537af69f678
Dimitrie Tataru <dimitriettr@gmail.com>
2024-03-12 18:54:44 +0200

    Update coding standards

--- src/clean-solution.bat
+++ src/clean-solution.bat
@@ -10,4 +10,10 @@ rmdir /s /q .\Ace.Geograpi.Infrastructure\obj
 rmdir /s /q .\Ace.Geograpi.Web\bin
 rmdir /s /q .\Ace.Geograpi.Web\obj
 
+rmdir /s /q .\tests\Ace.Geograpi.Application.Tests\bin
+rmdir /s /q .\tests\Ace.Geograpi.Application.Tests\obj
+
+rmdir /s /q .\tests\Ace.Geograpi.Infrastructure.IntegrationTests\bin
+rmdir /s /q .\tests\Ace.Geograpi.Infrastructure.IntegrationTests\obj
+
 pause
--- src/tests/Ace.Geograpi.Infrastructure.IntegrationTests/GlobalUsings.cs
+++ src/tests/Ace.Geograpi.Infrastructure.IntegrationTests/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using Ace.CSharp.StructuredAutoMapper.Abstractions.Test;
 global using FluentAssertions;
 global using Xunit;
+global using System;
+global using System.Collections.Generic;
