e3a358d400330bd28624a9239ccf13f92dba1208
Dimitrie Tataru <dimitriettr@gmail.com>
2024-05-14 18:52:17 +0300

    Add 'Docker' environment

--- src/Ace.Geograpi.Web/Program.cs
+++ src/Ace.Geograpi.Web/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddGeograpiSwagger();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Docker"))
 {
     app.UseGeograpiCorsPermissive();
     app.UseGeograpiSwagger();
