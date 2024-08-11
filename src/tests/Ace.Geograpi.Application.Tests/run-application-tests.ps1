9b92e992ead2ba7504ee2075039be38a06e8bc75
Dimitrie Tataru <dimitriettr@gmail.com>
2024-08-12 01:59:09 +0300

    Improve Ace.Geograpi.Application code cogerage reports

--- src/tests/Ace.Geograpi.Application.Tests/run-application-tests.ps1
+++
@@ -1,6 +0,0 @@
-dotnet test --collect:"XPlat Code Coverage" --settings .\runsettings --results-directory _CoverageResults;
-
-dotnet "$env:USERPROFILE\.nuget\packages\reportgenerator\5.3.8\tools\net8.0\ReportGenerator.dll" "-reports:_CoverageResults\*\coverage.cobertura.xml" "-targetdir:_CoverageReport\Application" "-historydir:_CoverageReport\_History\Application" -reporttypes:Html;
-dotnet "$env:USERPROFILE\.nuget\packages\reportgenerator\5.3.8\tools\net8.0\ReportGenerator.dll" "-reports:_CoverageResults\*\coverage.cobertura.xml" "-targetdir:_CoverageReport\Application" "-historydir:_CoverageReport\_History\Application" -reporttypes:Badges;
-
-Read-Host -Prompt "Press any key to continue.."
