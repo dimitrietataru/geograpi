21f39ece1668dd47f9da3c6083ceebeeb314547c
Dimitrie Tataru <dimitriettr@gmail.com>
2024-08-18 14:51:44 +0300

    Setup Ace.Grograpi.Infrastructure code coverage and report tools/scripts

---
+++ src/tests/Ace.Geograpi.Infrastructure.IntegrationTests/run-test-coverage.ps1
@@ -0,0 +1,9 @@
+$Project = "Ace.Geograpi.Infrastructure";
+$Root = "..\.cov";
+$ReportGeneratorPath = ".nuget\packages\reportgenerator\5.3.8\tools\net8.0\ReportGenerator.dll";
+
+dotnet test --collect:"XPlat Code Coverage" --settings .\runsettings --results-directory "$Root\raw\$Project";
+dotnet "$env:USERPROFILE\$ReportGeneratorPath" "-reports:$Root\raw\$Project\*\coverage.cobertura.xml" "-targetdir:$Root\reports\$Project" "-historydir:$Root\reports\.history\$Project" -reporttypes:Html;
+dotnet "$env:USERPROFILE\$ReportGeneratorPath" "-reports:$Root\raw\$Project\*\coverage.cobertura.xml" "-targetdir:$Root\reports\$Project" "-historydir:$Root\reports\.history\$Project" -reporttypes:Badges;
+
+Read-Host -Prompt "Press any key to continue.."
