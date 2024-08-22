7768e8c58631a6df3521ab66abe4590d3acf0d77
Dimitrie Tataru <dimitriettr@gmail.com>
2024-08-22 10:31:21 +0300

    Add Application.Web overrides, to properly determine code coverage

--- src/Ace.Geograpi.Web/Controllers/CountryController.cs
+++ src/Ace.Geograpi.Web/Controllers/CountryController.cs
@@ -2,7 +2,10 @@ using Ace.Geograpi.Domain.Models;
 using Ace.Geograpi.Domain.Models.Root;
 using Ace.Geograpi.Domain.QueryFilters;
 using Ace.Geograpi.Domain.Services;
+using CatNip.Domain.Query;
+using CatNip.Domain.Query.Sorting.Symbols;
 using CatNip.Presentation.Controllers;
+using CatNip.Presentation.Symbols;
 
 namespace Ace.Geograpi.Web.Controllers;
 
@@ -15,5 +18,65 @@ public sealed class CountryController : AceController<ICountryService, CountryMo
     public CountryController(ICountryService service)
         : base(service)
     {
+    }
+
+    [HttpGet]
+    [ProducesResponseType<QueryResponse<CountryRootModel>>((int)HttpStatusCode.OK)]
+    public sealed override async Task<IActionResult> GetAll(
+        [FromQuery] int? page,
+        [FromQuery] int? size,
+        [FromQuery] string? sortBy,
+        [FromQuery] SortDirection? sortDirection,
+        [FromQuery] CountryQueryFilter filter,
+        CancellationToken cancellation)
+    {
+        return await base.GetAll(page, size, sortBy, sortDirection, filter, cancellation);
+    }
+
+    [HttpGet]
+    [Route(DefaultRoutes.Count)]
+    [ProducesResponseType<int>((int)HttpStatusCode.OK)]
+    public sealed override async Task<IActionResult> Count(
+        [FromQuery] CountryQueryFilter filter, CancellationToken cancellation)
+    {
+        return await base.Count(filter, cancellation);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType<CountryModel>((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public sealed override async Task<IActionResult> GetById(
+        [FromRoute] int id, CancellationToken cancellation)
+    {
+        return await base.GetById(id, cancellation);
+    }
+
+    [HttpPost]
+    [ProducesResponseType<CountryModel>((int)HttpStatusCode.Created)]
+    public sealed override async Task<IActionResult> Create(
+        [FromBody] CountryModel model, CancellationToken cancellation)
+    {
+        return await base.Create(model, cancellation);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public sealed override async Task<IActionResult> Update(
+        [FromRoute] int id, [FromBody] CountryModel model, CancellationToken cancellation)
+    {
+        return await base.Update(id, model, cancellation);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType((int)HttpStatusCode.NotFound)]
+    public sealed override async Task<IActionResult> Delete(
+        [FromRoute] int id, CancellationToken cancellation)
+    {
+        return await base.Delete(id, cancellation);
     }
 }
